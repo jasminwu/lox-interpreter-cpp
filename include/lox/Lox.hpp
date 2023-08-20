164a95c89567332d0d8b41199ab3629862a52171
jiefu <z5476265@ad.unsw.edu.au>
2023-08-20 21:50:22 +1000

    wrote header file and stubs for lox, and wrote main

--- include/lox/Lox.hpp
+++ include/lox/Lox.hpp
@@ -1,1 +1,56 @@
 #define TEST_INCLUDE 69
+
+#include <string>
+
+#include "lox/Scanner.hpp"
+#include "lox/Token.hpp"
+#include "lox/TokenType.hpp"
+#include "lox/Parser.hpp"
+
+namespace lox 
+{
+
+class Lox {
+public:
+    static bool hadError_;
+
+    /**
+     * @brief
+     * 
+     * @param source 
+     */
+    static void runFile(std::string source);
+
+    /**
+     * @brief 
+     * 
+     */
+    static void runPrompt();
+
+    /**
+     * @brief 
+     * 
+     * @param source 
+     */
+    static void run(std::string source);
+
+private:
+    /**
+     * @brief 
+     * 
+     * @param line 
+     * @param message 
+     */
+    static void error(int line, std::string message);
+
+    /**
+     * @brief 
+     * 
+     * @param line 
+     * @param where 
+     * @param message 
+     */
+    static void report(int line, std::string where, std::string message);
+};
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,20 @@
 
 #include "lox/Lox.hpp"
 
-int main() {
-    std::cout << "Hello World! \n";
-    std::cout << "Hello World! \n";
-    std::cout << TEST_INCLUDE;
+int main(int argc, char **argv) {
+    std::cout << "Lox Interpreter - Iteration 1 \n";
+    std::cout << "by Jasmin and Jeff \n";
+
+    if (argc > 2) {
+        std::cout << "Usage: app [script]\n";
+        return 1;
+    } else if (argc == 2) {
+        std::cout << "Preparing to run file...\n";
+        lox::Lox::runFile(argv[1]);
+    } else {
+        std::cout << "Initialising command prompt...\n";
+        lox::Lox::runPrompt();
+    }
+
     return 0;
 }
