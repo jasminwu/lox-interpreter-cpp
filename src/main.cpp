e422d8a2c16c1afce357a8d92df737395f751140
jiefu <z5476265@ad.unsw.edu.au>
2023-08-20 13:38:17 +1000

    added iter1 starter code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 
+#include "Lox.hpp"
+
 int main() {
     std::cout << "Hello World! \n";
     std::cout << "Hello World! \n";
+    std::cout << TEST_INCLUDE;
     return 0;
 }
---
+++ test/Parser.test.cpp
@@ -0,0 +1,7 @@
+// Instead of using Lox::Scanner to generate input for our Parser for our test
+// we manually create our own vectors of tokens
+
+/*
+auto testTokens1 = new std::vector<Token>();
+testTokens.push_back(Token(LEFT_PAREN, "(", NULL, 0));
+*/
