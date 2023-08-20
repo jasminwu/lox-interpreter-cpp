59a1243cfedb68616d1714a4e1ccd591c36abc55
jiefu <z5476265@ad.unsw.edu.au>
2023-08-20 23:36:15 +1000

    wrote function prototypes for some testutil functions

---
+++ test/testutils/utils.hpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+#include "lox/Lox.hpp"
+#include "lox/Scanner.hpp"
+#include "lox/Token.hpp"
+#include "lox/TokenType.hpp"
+#include "lox/Parser.hpp"
+
+// Implement all helper functions here as static functions
+
+namespace lox
+{
+namespace test
+{
+
+// function prototypes
+static bool TokensEqual(Token a, Token b);
+static bool TokenListsEqual(std::vector<Token> a, std::vector<Token> b);
+
+
+// function definitions
+
+}
+}
