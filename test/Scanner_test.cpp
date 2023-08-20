1dae8052a4ca07b793d95fa6c1d7a96824a001ba
jiefu <z5476265@ad.unsw.edu.au>
2023-08-20 14:46:23 +1000

    working on testing framework

--- test/Scanner_test.cpp
+++ test/Scanner_test.cpp
@@ -1,3 +1,11 @@
-int main() {
-    return 1;
+#include "catch2/catch.hpp"
+
+TEST_CASE("Does math work?") {
+    CHECK((1 + 1) == 2);
+    CHECK((2 + 2) == 4);
+}
+
+TEST_CASE("UWU?") {
+    CHECK((3 + 3) == 6);
+    CHECK(2 == 2);
 }
