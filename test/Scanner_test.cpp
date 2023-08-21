d0f8140786b9906fcaf0c7e1d139ef953b0ee0e6
jiefu <z5476265@ad.unsw.edu.au>
2023-08-22 01:27:03 +1000

    changed variant to include monostate

--- test/Scanner_test.cpp
+++ test/Scanner_test.cpp
@@ -1,4 +1,30 @@
+#include <string>
+#include <variant>
+
+#include "lox/Lox.hpp"
+#include "lox/Token.hpp"
+#include "lox/TokenType.hpp"
+#include "lox/Scanner.hpp"
+
 #include "catch2/catch.hpp"
 #include "testutils/utils.hpp"
 
 TEST_CASE("YOU HAVE NOT WRITTEN TESTS FOR SCANNER!!") { CHECK((2 + 2) == 4); }
+
+TEST_CASE("Correctly scans a simple hello world program") {
+    std::string source = "print \"Hello, world!\";";
+
+    // build the expected token list
+    auto expected = std::vector<lox::Token>();
+
+    // manually creating the token list using initialiser lists
+    /*
+    expected.push_back({
+        PRINT,
+        "print",
+        SOMETHING IDK,
+        1
+    });
+    */
+   CHECK(1 == 1);
+}
